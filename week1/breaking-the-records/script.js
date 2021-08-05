360338d3564feaf7104e3e641d92cb6efc1ef32e
Seventty <elmastigeron2015@gmail.com>
2021-08-04 21:05:09 -0400

    Excercise solved

--- week1/breaking-the-records/script.js
+++ week1/breaking-the-records/script.js
@@ -1,1 +1,32 @@
-.
+/* Breaking the Records */
+
+/** 
+* @param array
+* * miniMaxSum has the following parameter(scores):
+* * arr: an array of 5 scores(integers)
+*/
+
+function breakingRecords(scores) {
+    let minimum = scores[0],
+    maximum = scores[0],
+    min = 0,
+    max = 0,
+    res = [];
+
+    if (scores.length < 1 || scores.length > 1000) throw "Scores must be between 2 and 1000";
+    for (const i of scores) {
+        if(i > maximum){
+            maximum = i;
+            max++;
+        }
+        if(i < minimum){
+            minimum = i;
+            min++;
+        }
+    };
+    res = [...res, max, min];
+    return res;
+}
+
+const scores = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+breakingRecords(scores);
